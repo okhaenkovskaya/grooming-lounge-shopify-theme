5ce2aea20befb04cbe0c67117a4b1087ef70c595
Oksana Prokopenko <oksana.prokopenko@p2h.com>
2020-12-21 11:04:26 +0200

    Added files from the live theme

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=8de030f3-582c-4abe-8068-d03694462030';document.getElementsByTagName('head')[0].appendChild(script);
